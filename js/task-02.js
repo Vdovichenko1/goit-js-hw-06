68f353f152c40c22aa4da8b9c7d61baf5f738d55
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-09-22 17:39:53 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,23 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 const ulIngredients = document.querySelector('#ingredients');
 
-for (let el of ingredients) {
-  const ingredientListEl = document.createElement('li');
-  ingredientListEl.textContent = el;
-  ingredientListEl.classList.add('item');
-  ulIngredients.append(ingredientListEl);
+// for (let el of ingredients) {
+//   const ingredientListEl = document.createElement('li');
+//   ingredientListEl.textContent = el;
+//   ingredientListEl.classList.add('item');
+//   ulIngredients.append(ingredientListEl);
+// }
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const option = ingredients[i];
+  const list = document.createElement('li');
+  list.textContent = option;
+  list.classList.add('item');
+
+  elements.push(list);
 }
 
+ingredientEl.append(...elements);
